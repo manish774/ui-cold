01b54905b9fece534a79fa8c2170eabc0d13bfb3
manish774 <manishranbir774@gmail.com>
2024-02-25 14:11:10 +0530

    feat(adding github workflow to do semantic releases): semantic release code
    Semantic release code by me

---
+++ src/components/ImageZoomer/index.ts
@@ -0,0 +1,1 @@
+export { default as ImageZoomer } from "./ImageZoomer";
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,14 @@ import { Badge, CircleBadge, BadgeModel } from "./Badge";
 import { Breadcrumbs } from "./Breadcrumbs";
 import { Files } from "./Files";
 import { Input, Select, TableFooter, TableHeader } from "./generic";
+import { Tree } from "./Tree";
+import { Tooltip } from "./Tooltip";
+import { Toast } from "./Toast";
+import { Table } from "./Table";
+import { Skeleton } from "./Skeleton";
+import { Slider } from "./Silder";
+import { Panel } from "./panel";
+import { Modal } from "./Modal";
 
 export {
   Copy,
@@ -15,4 +23,12 @@ export {
   Select,
   TableFooter,
   TableHeader,
+  Tree,
+  Tooltip,
+  Toast,
+  Table,
+  Skeleton,
+  Slider,
+  Panel,
+  Modal,
 };
